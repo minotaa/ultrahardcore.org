84f6264a93362ad0a953924c84674d3a1977de79
Juan <jpichardo369@gmail.com>
2022-11-03 21:04:11 -0400

    Frontend experimentation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,8 +5,10 @@ import ServerSelector from '../components/ServerSelector'
 export default function() {
   return (
     <>
-      <Button href="https://example.com" text="hello world!"/>
-      <ServerSelector text="applejuice.bar"/>
+      <div className="container">
+        <Button href="https://example.com" text="hello world!"/>
+        <ServerSelector text="applejuice.bar"/>
+      </div>
     </>
   )
 }
