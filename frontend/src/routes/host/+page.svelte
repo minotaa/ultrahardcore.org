84118e4189cf5cc070e694962f6dcc31bdc66a70
Juan <jpichardo369@gmail.com>
2023-03-01 01:15:51 -0500

    Do more work on date selecting

--- frontend/src/routes/host/+page.svelte
+++ frontend/src/routes/host/+page.svelte
@@ -70,6 +70,18 @@
     checkConflicts()
   }
 
+  function setDate(day: string) {
+    desiredTime.date(parseInt(day))
+    desiredTime = desiredTime
+    checkConflicts()
+  }
+
+  function setDateNextMonth(month: number, day: number) {
+    desiredTime.set({ 'month': month, 'date': day })
+    desiredTime = desiredTime
+    checkConflicts()
+  }
+
   function checkConflicts() {
     errors = []
     if (desiredTime.isBefore(moment())) { 
@@ -85,16 +97,49 @@
   let errors: string[] = []
   checkConflicts()
   let thisMonth = calendarize(moment().toDate())
+  let thisMonthLabel = moment().format("MMMM YYYY")
   let nextMonth = calendarize(moment().add(1, 'months').toDate())
+  let nm = moment().add(1, 'months')
+  let nextMonthLabel = moment().add(1, 'months').format("MMMM YYYY")
 </script>
 
 <div class="container pl-8">
   <h1 class="pt-6 font-bold text-2xl dark:text-white">Create Match</h1>
   <h2 class="font-bold text-xl dark:text-white">desired: {desiredTime.format("MMM Do, HH:mm")}</h2>
-  <div class="flex flex-col">
+  <div class="flex flex-col justify-center">
     {#if errors.length > 0}
-      <h2 class="text-center text-md bg-red-100 dark:text-white dark:bg-red-500 shadow rounded-lg pt-2 pb-2 pr-8 pl-8 mb-4"><strong>Error:</strong> {errors.join(', ')}</h2>
+      <h2 class="text-center mt-2 text-md bg-red-100 dark:text-white dark:bg-red-500 shadow rounded-lg pt-2 pb-2 pr-8 pl-8 mb-4"><strong>Error:</strong> {errors.join(', ')}</h2>
     {/if}
+    <div class="flex flex-row gap-6 justify-center">
+      <div class="flex flex-col rounded dark:border-slate-600 bg-slate-200 dark:bg-slate-800 border-2 w-fit">
+        <h1 class="text-lg dark:text-white text-center font-bold p-2">{thisMonthLabel}</h1>
+        {#each thisMonth as week}
+          <div class="flex flex-row">
+            {#each week as day}
+              {#if day == 0}
+                <button class="p-2 w-12 h-12 dark:text-white border-slate-200 dark:border-slate-700 bg-slate-200 dark:bg-slate-800">&nbsp;</button> 
+              {:else}
+                <button class="p-2 w-12 h-12 dark:text-white border-slate-200 dark:border-slate-700 bg-slate-200 dark:bg-slate-800">{day}</button> 
+              {/if}
+            {/each}
+          </div>
+        {/each}
+      </div>
+      <div class="flex flex-col rounded dark:border-slate-600 bg-slate-200 dark:bg-slate-800 border-2 w-fit">
+        <h1 class="text-lg dark:text-white text-center font-bold p-2">{nextMonthLabel}</h1>
+        {#each nextMonth as week}
+          <div class="flex flex-row">
+            {#each week as day}
+              {#if day == 0}
+                <button class="p-2 w-12 h-12 dark:text-white border-slate-200 dark:border-slate-700 bg-slate-200 dark:bg-slate-800">&nbsp;</button> 
+              {:else}
+                <button class="p-2 w-12 hover:bg-zinc-900 h-12 dark:text-white border-slate-200 dark:border-slate-700 bg-slate-200 dark:bg-slate-800">{day}</button> 
+              {/if}
+            {/each}
+          </div>
+        {/each}
+      </div>
+    </div>
     <div class="flex-row gap-4">
       <h3 class="border-2 border-slate-50 select-none border-slate-200 dark:border-slate-700 text-xl dark:text-white pt-2 pb-2 pl-8 pr-8 bg-slate-100 dark:bg-slate-800 rounded-lg text-center w-fit mt-2">{desiredTime.format("HH:mm")}</h3>
       <div class="gap-2 border-slate-200 flex-row flex mt-2 border-2 bg-slate-100 dark:bg-slate-800 select-none dark:border-slate-700 rounded-lg pt-2 pb-2 pl-4 pr-4 w-fit text-md dark:text-white">
@@ -119,38 +164,6 @@
               {/if}
             {/each}
           </ul>
-        </div>
-      </div>
-      <div class="flex flex-row gap-4">
-        <div class="flex flex-col border-slate-200 border-2 dark:border-slate-900 dark:bg-slate-800 bg-slate-200 rounded-lg p-2">
-          {#each thisMonth as week}
-            <div class="flex flex-row">
-              {#each week as day} 
-                {#if day == 0}
-                  <h2 class="select-none text-lg p-3 bg-slate-100 dark:bg-slate-800 rounded">&nbsp; &nbsp;</h2>
-                {:else}
-                  <h2 class="select-none text-lg dark:hover:bg-slate-900 border border-slate-700 bg-slate-100 dark:bg-slate-800 dark:text-white p-3">{day}</h2>
-                {/if}
-              {/each}
-            </div>
-          {/each}
-        </div>
-        <div class="flex flex-col border-slate-200 dark:border-slate-900 border-2 dark:bg-slate-800 bg-slate-200 rounded-lg p-2">
-          {#each nextMonth as week}
-            <div class="flex flex-row">
-              {#each week as day} 
-                {#if day == 0}
-                  <h2 class="select-none text-lg border border-slate-700 bg-slate-100 dark:bg-slate-800 dark:text-white pl-3 pr-3">&nbsp;&nbsp;&nbsp;</h2>
-                {:else}
-                  <div class="select-none dark:hover:bg-slate-900 border border-slate-700 bg-slate-100 dark:bg-slate-800 p-3">
-                    <p class="text-lg dark:text-white">
-                      {day}
-                    </p>
-                  </div>
-                {/if}
-              {/each}
-            </div>
-          {/each}
         </div>
       </div>
     </div>
